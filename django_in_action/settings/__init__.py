a320d431a1e03f948af08cf0a11f46af151e7faf
echo1937 <1qaz@WSX3edc>
2022-03-24 22:28:13 +0800

    - 增加了simplejwt, 包括依赖、url、settings、INSTALLED_APPS - 修改了DEFAULT_AUTHENTICATION_CLASSES - 增加了DEFAULT_PERMISSION_CLASSES

--- django_in_action/settings/__init__.py
+++ django_in_action/settings/__init__.py
@@ -1,6 +1,7 @@
 import os
 from .settings import *
 from .rest_framework import *
+from .simplejwt import *
 
 if os.environ.get('PROFILE_TYPE') == 'production':
     from .production import *
--- django_in_action/urls.py
+++ django_in_action/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 # Serializers define the API representation.
@@ -37,7 +38,11 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('admin/', admin.site.urls),  # admin
+    path('api-auth/', include('rest_framework.urls')),  # rest_framework
     path('', include(router.urls)),
+    # simple JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
